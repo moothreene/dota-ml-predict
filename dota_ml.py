ca84853058d485a145def5b1b12852cef6875470
Martin Belyj <moothreene@gmail.com>
2023-08-09 14:56:10 +0300

    Tidying up

--- dota_ml.py
+++ dota_ml.py
@@ -1,48 +1,90 @@
-#from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_absolute_error
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
 import pandas as pd
-import openpyxl
 
-def train_model(data, col):
-    y = data[col]
-    ids = data['match_id']
-    X = data.drop([col,'match_id'], axis = 1)
+import openpyxl
+from sklearn.model_selection import train_test_split
+
+#OH encodes data that was split for evaluation purposes and returns encoded data
+#X_train, X_valid - results of train_test_split method
+def OH_encode_train(X_train, X_valid):
+
+    #setting up the encoder and getting a list of object columnd
+    OH_encoder = OneHotEncoder(handle_unknown = 'ignore', sparse = False)
+    object_columns = (X_train.dtypes == 'object')
+    object_columns = list(object_columns[object_columns].index)
+
+    #OH encoding object columns
+    OH_X_train_col = pd.DataFrame(OH_encoder.fit_transform(X_train[object_columns]))
+    OH_X_valid_col = pd.DataFrame(OH_encoder.transform(X_valid[object_columns]))
+
+    #bringing back an index, dropped by OH encoding and making sure that encoded columns are of str type
+    OH_X_train_col.index = X_train.index
+    OH_X_valid_col.index = X_valid.index
+    OH_X_train_col.columns = OH_X_train_col.columns.astype(str)
+    OH_X_valid_col.columns = OH_X_valid_col.columns.astype(str)
+
+    #getting number columns from original DataFrame and adding them to encoded object columns
+    num_X_train_col = X_train.drop(object_columns, axis = 1)
+    num_X_valid_col = X_valid.drop(object_columns, axis = 1)
+    X_train_full = pd.concat([num_X_train_col, OH_X_train_col], axis = 1)
+    X_valid_full = pd.concat([num_X_valid_col, OH_X_valid_col], axis = 1)
+
+    return X_train_full, X_valid_full
+    
+#OH encodes data and returns encoded data
+#X - data for encoding
+def OH_encode(X):
+    #setting up the encoder and getting a list of object columnd
+    OH_encoder = OneHotEncoder(handle_unknown = 'ignore', sparse = False)
     object_columns = (X.dtypes == 'object')
     object_columns = list(object_columns[object_columns].index)
-    OH_encoder = OneHotEncoder(handle_unknown = 'ignore', sparse = False)
-    #train_X, val_X, train_y, val_y = train_test_split(X, y, random_state= 0)
 
-    #OH_train_X_col = pd.DataFrame(OH_encoder.fit_transform(train_X[object_columns]))
-    #OH_val_X_col = pd.DataFrame(OH_encoder.transform(val_X[object_columns]))
+    #OH encoding object columns
     OH_X_col = pd.DataFrame(OH_encoder.fit_transform(X[object_columns]))
 
-    #OH_train_X_col.index = train_X.index
-    #OH_val_X_col.index = val_X.index
+    #bringing back an index, dropped by OH encoding and making sure that encoded columns are of str type
     OH_X_col.index = X.index
+    OH_X_col.columns = OH_X_col.columns.astype(str)
 
-    #num_train_X = train_X.drop(object_columns, axis = 1)
-    #num_val_X = val_X.drop(object_columns, axis = 1)
-    num_X = X.drop(object_columns, axis = 1)
+    #getting number columns from original DataFrame and adding them to encoded object columns
+    num_X_col = X.drop(object_columns, axis = 1)
+    X_full = pd.concat([num_X_col, OH_X_col], axis = 1)
 
-    #OH_train_X = pd.concat([num_train_X, OH_train_X_col], axis = 1)
-    #OH_val_X = pd.concat([num_val_X, OH_val_X_col], axis = 1)
-    OH_X = pd.concat([num_X, OH_X_col], axis = 1)
+    return X_full
 
-    #OH_train_X.columns = OH_train_X.columns.astype(str)
-    #OH_val_X.columns = OH_val_X.columns.astype(str)
-    OH_X.columns = OH_X.columns.astype(str)
+#sets up and trains machine learning model on given data, saving results as excel file
+#data - data to train on, col - target column
+def train_model(data, col):
 
+    #dividing DataFrame into y - target column and X - the rest
+    X = data.drop([col,'match_id'], axis = 1)
+    y = data[col]
+    
+    #X_train, X_valid, y_train, y_valid = train_test_split(X, y, random_state= 0)  - for mae validation
+
+    #OneHotEncoding X
+    OH_X = OH_encode(X)
+    #OH_X_train, OH_X_valid = OH_encode_train(X_train, X_valid) - for mae validation
+
+    #setting up ml model
     model = RandomForestRegressor(n_estimators=200, min_samples_split=20, random_state=0)
 
-    #print(score_model(model,OH_train_X, OH_val_X, train_y, val_y))
+    #print(score_model(model,OH_X_train, OH_X_valid, y_train, y_valid)) - for mae validation
 
+    #fitting data into model
     model.fit(OH_X, y)
+
+    #making predictions and saving them as excel file
     preds = model.predict(OH_X)
-    out = pd.concat([ids, pd.DataFrame(preds, columns = ['Prediction']), y ], axis = 1)
+    out = pd.concat([data['match_id'], pd.DataFrame(preds, columns = ['Prediction']), y ], axis = 1)
     out.to_excel('Preds.xlsx')
 
+#returns mae score for a model
+#model - model to score, X_t, X_v, y_t, y_v - results of train_test_split method
 def score_model(model, X_t, X_v, y_t, y_v):
     model.fit(X_t, y_t)
     preds = model.predict(X_v)
--- dota_parse_picks.py
+++ dota_parse_picks.py
@@ -1,23 +1,33 @@
 import pandas as pd
 
-def dict_to_teamlist(row, heroes):
+#reads a dictionary of picks/bans and returns a str array, containing heroes names, according to their id
+#row - row in Pandas DataFrame, heroes - dictionary with heroes names and ids
+def dict_to_herolist(row, heroes):
+
     picks_data = row['picks_bans']
     team_radiant = []
     team_dire = []
 
     if row['picks_bans'] != None:
         for pick_ban in picks_data:
+
+            #selecting only picked heroes
             if pick_ban['is_pick'] == True:
                 hero = next(hero["name"] for hero in heroes if hero["id"] == pick_ban['hero_id'])
+
+                #dividing picked heroes by their respective team, 0 being radiant and 1 dire
                 if pick_ban['team'] == 0:
                     team_radiant.append(hero)
                 else:
                     team_dire.append(hero)
 
+    #adding arrays with hero names to DataFrame row
     row['picks_radiant'] = team_radiant
     row['picks_dire'] = team_dire
     return row 
 
+#splits a columns containing same size arrays into columns containing these arrays' elements
+#df - DataFrame, col_name - name of a target column, num - length of arrays in this column
 def split_array_col(df, col_name, num):
     df[[col_name + '_' + str(x + 1) for x in range(num)]] = pd.DataFrame(df[col_name].tolist(), index= df.index)
     return
