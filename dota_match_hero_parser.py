d156e34143a93de4ce009f338cd8d491f9baf65f
Martin Belyj <moothreene@gmail.com>
2023-08-20 16:03:29 +0300

    matchups remade based on parsed pro matches

--- dota_match_hero_parser.py
+++ dota_match_hero_parser.py
@@ -1,5 +1,6 @@
 import json
 import os
+import pandas as pd
 from dota_ml import HEROES_JSON
 
 MATCHES_JSON = json.load(open('parsed_matches.json'))
@@ -7,23 +8,23 @@ MATCHES_JSON = json.load(open('parsed_matches.json'))
 
 
 
-def create_winrates_empty(heroes):
+def create_matchups_empty(heroes):
 
-    winrates_final = {}
+    matchups_final = {}
 
     for hero in heroes:
 
         hero_id = hero["id"]
-        winrates_inside = {}
+        matchups_internal = {}
         for hero_inside in heroes:
             hero_id_inside = hero_inside["id"]
-            winrates_inside[hero_id_inside] = {"with" : {"wins" : 0, "total" : 0}, "against" : {"wins" : 0, "total" : 0}}
-        winrates_final[hero_id] = winrates_inside
+            matchups_internal[hero_id_inside] = {"with" : {"wins" : 0, "total" : 0}, "against" : {"wins" : 0, "total" : 0}}
+        matchups_final[hero_id] = matchups_internal
     
-    return winrates_final
+    return matchups_final
 
 
-def get_parsed_winrates(matches, winrates):
+def get_parsed_matchups(matches, matchups):
     num = 0
 
     for match in matches:
@@ -46,21 +47,75 @@ def get_parsed_winrates(matches, winrates):
 
                         if pick_first["team"] == pick_second["team"]:
 
-                            winrates[pick_first_hero_id][pick_second_hero_id]["with"]["total"] += 1
+                            matchups[pick_first_hero_id][pick_second_hero_id]["with"]["total"] += 1
 
                             if pick_first["team"] == int(not match["radiant_win"]):
 
-                                    winrates[pick_first_hero_id][pick_second_hero_id]["with"]["wins"] += 1
+                                    matchups[pick_first_hero_id][pick_second_hero_id]["with"]["wins"] += 1
                         else:
 
-                            winrates[pick_first_hero_id][pick_second_hero_id]["against"]["total"] += 1
+                            matchups[pick_first_hero_id][pick_second_hero_id]["against"]["total"] += 1
 
                             if pick_first["team"] == int(not match["radiant_win"]):
 
-                                winrates[pick_first_hero_id][pick_second_hero_id]["against"]["wins"] += 1
+                                matchups[pick_first_hero_id][pick_second_hero_id]["against"]["wins"] += 1
                         
-                                      
-winrates = create_winrates_empty(HEROES_JSON)
-get_parsed_winrates(MATCHES_JSON, winrates)
-file = open("winrates_final.json","w")
-file.write(json.dumps(winrates, indent=2))
+
+
+def hero_matchups_to_winrates(matchups):
+
+    CONST_WINRATE = 5
+
+    winrates_percentage = {}
+
+    for hero_id in matchups:
+
+        winrates_per_hero = {}
+
+        for hero_id_internal in matchups[hero_id]:
+
+            stats = matchups[hero_id][hero_id_internal]
+
+            if stats["with"]["total"] != 0:
+
+                winrate_with = (stats["with"]["wins"] + 5) / (stats["with"]["total"] + 10)
+            else:
+
+                winrate_with = 0.5
+
+            if stats["against"]["total"] != 0:
+
+                winrate_against = (stats["against"]["wins"] + 5) / (stats["against"]["total"] + 10)
+            else:
+                winrate_against = 0.5
+
+            winrates_per_hero[hero_id_internal] = {"with" : winrate_with, "against" : winrate_against}
+
+        winrates_percentage[hero_id] = winrates_per_hero
+
+    return winrates_percentage
+
+
+'''matchups = create_matchups_empty(HEROES_JSON)
+get_parsed_matchups(MATCHES_JSON, matchups)
+file = open("dota_hero_matchups.json","w")
+file.write(json.dumps(matchups, indent = 2))'''
+
+WINRATES_JSON = json.load(open('dota_hero_matchups.json'))
+
+winrates = hero_matchups_to_winrates(WINRATES_JSON)
+file = open("dota_winrates_by_hero.json", "w")
+file.write(json.dumps(winrates, indent = 2))
+
+
+'''heroes_with = []
+
+for hero_id in winrates:
+    hero_array = []
+    for hero_id_internal in winrates[hero_id]:
+        hero_array.append(winrates[hero_id][hero_id_internal]["with"])
+    heroes_with.append(hero_array)
+
+df = pd.DataFrame(heroes_with)
+df.columns = [hero["id"] for hero in HEROES_JSON]
+df.to_excel("winrates.xlsx")'''
