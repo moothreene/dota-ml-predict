dd1c205d854d36e68d34fca57df774db8a07dde1
Martin Belyj <moothreene@gmail.com>
2023-08-22 14:10:35 +0300

    cleanup

--- opendota_api.py
+++ opendota_api.py
@@ -4,6 +4,41 @@ import time
 import os
 
 
+#sql queries for explorer function
+QUERY_LEAGUE = """
+                    SELECT
+                    match_id,
+                    radiant_win,
+                    radiant_team_id,
+                    dire_team_id,
+                    picks_bans
+                    FROM matches
+                    JOIN leagues using(leagueid)
+                    WHERE TRUE
+                    AND matches.leagueid = 15475
+                    AND matches.start_time >= extract(epoch from timestamp '2023-07-05T16:00:51.067Z')
+                    GROUP BY matches.match_id
+                    HAVING count(distinct match_id) >= 1
+                    ORDER BY matches.match_id
+                """
+
+
+QUERY_2022 = """
+                    SELECT
+                    match_id,
+                    radiant_win,
+                    picks_bans
+                    FROM matches
+                    WHERE TRUE
+                    AND matches.start_time >= extract(epoch from timestamp '2022-01-01T00:00:00.000Z')
+                    AND NOT picks_bans IS NULL
+                    GROUP BY matches.match_id
+                    HAVING count(distinct match_id) >= 1
+                    ORDER BY matches.match_id
+                """
+
+
+
 def get_matchups_wr(id, matchups):
     matchups_wr = {'id' : id}
     for matchup in matchups:
@@ -63,45 +98,15 @@ def heroes():
     file.write(json.dumps(out, indent=2))
 
 
-def explorer():
-    query_league = """
-                    SELECT
-                    match_id,
-                    radiant_win,
-                    radiant_team_id,
-                    dire_team_id,
-                    picks_bans
-                    FROM matches
-                    JOIN leagues using(leagueid)
-                    WHERE TRUE
-                    AND matches.leagueid = 15475
-                    AND matches.start_time >= extract(epoch from timestamp '2023-07-05T16:00:51.067Z')
-                    GROUP BY matches.match_id
-                    HAVING count(distinct match_id) >= 1
-                    ORDER BY matches.match_id
-                """
-    query_2022 = """
-                    SELECT
-                    match_id,
-                    radiant_win,
-                    picks_bans
-                    FROM matches
-                    WHERE TRUE
-                    AND matches.start_time >= extract(epoch from timestamp '2022-01-01T00:00:00.000Z')
-                    AND NOT picks_bans IS NULL
-                    GROUP BY matches.match_id
-                    HAVING count(distinct match_id) >= 1
-                    ORDER BY matches.match_id
-                """
+def explorer(query):
 
-    params_league = {"sql" : query_league}
-    params_reg = {"sql" : query_2022}
+    params = {"sql" : query}
     url = 'https://api.opendota.com/api/explorer'
-    response = requests.get(url, params = params_reg)
+    response = requests.get(url, params = params)
     out = response.json()
     print(out["rowCount"])
     file = open("parsed_matches.json","w")
     file.write(json.dumps(out["rows"], indent=2))
 
 
-explorer()
+explorer(QUERY_2022)
